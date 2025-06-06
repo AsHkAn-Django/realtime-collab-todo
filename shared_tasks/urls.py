a694787745a90e6c22827e7d3beda19703df2716
AsHkaN <ashkan_ahrari@yahoo.com>
2025-06-06 19:03:37 +0300

    delete subtask view added and restricted just for owner of the project

--- shared_tasks/urls.py
+++ shared_tasks/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('participation_request/<int:pk>/', views.ParticipationRequestCreateView.as_view(), name='participation_request'),
     path('request_project_list/', views.RequestProjectListView.as_view(), name='request_project_list'),
     path('request_project_list/edit/<int:pk>/', views.SharedSubTaskUpdateView.as_view(), name='shared_subtask_update'),
+    path('request_project_list/delete/<int:pk>/', views.SharedSubTaskDeleteView.as_view(), name='shared_subtask_delete'),
     path('request_project_list/accept/<int:pk>/', views.accept_user, name='accept_user'),
     path('request_project_list/refuse/<int:pk>/', views.refuse_user, name='refuse_user'),
 
--- shared_tasks/views.py
+++ shared_tasks/views.py
@@ -1,7 +1,8 @@
 from django.views import generic
 from .models import TaskRecord, SubTask, SharedTask, ParticipationRequest
 from .forms import TaskRecordForm, SubTaskForm, ShareTaskForm, ParticipationRequestForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
@@ -21,12 +22,22 @@ class SharedTaskListView(LoginRequiredMixin, generic.ListView):
             task.show_participate_button = (user != task.creator and user not in participants)
             task.show_edit_button = (user == task.creator or user in participants)
             task.show_delete_button = (user == task.creator)
-
         return queryset
     
-    
 
-class SharedSubTaskUpdateView(generic.UpdateView):
+
+class SharedSubTaskDeleteView(UserPassesTestMixin, LoginRequiredMixin, generic.DeleteView):
+    model = SubTask
+    template_name = "shared_task/shared_subtask_delete.html"
+    success_url = reverse_lazy('shared_task:shared_task_list')
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.creator == self.request.user
+
+   
+
+class SharedSubTaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = SubTask
     form_class = SubTaskForm
     template_name = "shared_task/shared_subtask_update.html"
@@ -34,7 +45,7 @@ class SharedSubTaskUpdateView(generic.UpdateView):
 
 
 
-class ParticipationRequestCreateView(generic.CreateView):
+class ParticipationRequestCreateView(LoginRequiredMixin, generic.CreateView):
     model = ParticipationRequest
     form_class = ParticipationRequestForm
     template_name = "shared_task/participation_request.html"
@@ -51,18 +62,23 @@ class ParticipationRequestCreateView(generic.CreateView):
         messages.success(self.request, 'Your request has been sent successfully. The project owner will inform you ASAP.')
         return super().form_valid(form)
     
+    
 
-class RequestProjectListView(generic.ListView):
+class RequestProjectListView(LoginRequiredMixin, generic.ListView):
     model = ParticipationRequest
     template_name = "shared_task/request_project_list.html"
     context_object_name = "requests"
 
 
+
+@login_required
 def accept_user(request, pk):
     request = get_object_or_404(ParticipationRequest, pk=pk)
     request.accept()
     return redirect('shared_task:request_project_list')
 
+
+@login_required
 def refuse_user(request, pk):
     request = get_object_or_404(ParticipationRequest, pk=pk)
     request.refuse()
